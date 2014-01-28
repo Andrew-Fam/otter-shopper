67cfd8f77d19a5917c96b7ccd600529e9b1d6009
Andrew Fam <whatsup@andrewfam.com>
2014-01-28 23:23:22 +1100

    auto commit (mixture)

---
+++ public/js/otter.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+	$('#causes-indicators li').click( function (){
+		$('#causes-indicators li').removeClass('active');
+		$(this).addClass('active');
+	});
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+});
